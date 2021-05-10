065893e2cf9ec3c22359b59754de7126c6aa5a9a
Tophia <76433173+Tophiak@users.noreply.github.com>
2021-05-10 12:33:35 +0530

    scdcmm

--- api/index.js
+++ api/index.js
@@ -1,16 +1,13 @@
 
-const mongoose = require('mongoose');
-const users = require('./routes/user');
-const express = require('express');
-const app = express();
 const express = require('express');
 const path = require('path');
 const app = express(),
       bodyParser = require("body-parser");
       port = 80;
 
+
 // place holder for the data
-const users = [
+const usersdata = [
   {
     Company: "first1",
     Market_Cap: "11111",
@@ -53,9 +50,9 @@ const users = [
 ];
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../my-app/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/api/users', (req, res) => {
+app.get('/api/user', (req, res) => {
   console.log('api/users called!')
   res.json(users);
 });
@@ -68,21 +65,9 @@ app.post('/api/user', (req, res) => {
 });
 
 app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
 });
-mongoose.connect("mongodb://localhost:27017/Riafy", { useNewUrlParser: true,useUnifiedTopology: true })
-.then(res => console.log('Connected to db'))
-    .then(() => console.log('Now connected to MongoDB!'))
-    .catch(err => console.error('Something went wrong', err));
-
-app.use(express.json());
-app.use('/api/users', users);
-
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
- 
- 
